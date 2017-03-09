22947a4c90230abfe04fcac2fb3095d63b69cc36
Joe Bumbulis <joebumbulis@gmail.com>
2017-03-09 14:26:05 -0600

    finished html tree, started javascript

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log('hi');
+
+ //output the result of two number values mathed
+ // take the value of one number button, apply a math operator to another number button value.
+ //press calc button to make the two selected numbers + - * / then output.
+
+//output the value of two number button values that have been mathed
+//take 'textContent' from the clicking of a button
+// then apply a math operator
+//then take another 'textContent' from clicking of a button
+//output the result
+var ONE = document.querySelector('#one');
+console.dir (ONE);
+var TWO = document.querySelector('#');
+var THREE = document.querySelector('#one');
+var FOUR = document.querySelector('#one');
+var FIVE = document.querySelector('#one');
+var SIX = document.querySelector('#one');
+var SEVEN = document.querySelector('#one');
+var EIGHT = document.querySelector('#one');
+var NINE = document.querySelector('#one');
+var ZERO = document.querySelector('#one');
