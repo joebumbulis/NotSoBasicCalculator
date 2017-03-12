1e9a8c6b54bd488f7b875452f7619b57a089c85c
Joe Bumbulis <joebumbulis@gmail.com>
2017-03-12 13:02:16 -0500

    created clear button

--- main.js
+++ main.js
@@ -35,6 +35,7 @@ console.dir(plusBtn);
 
 var output = document.querySelector('#output');
 var calcBtn = document.querySelector('#calc');
+var clearBtn =document.querySelector('#clear');
 
 // var numbers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
 var zeroClick = 0;
@@ -125,6 +126,9 @@ nineBtn.addEventListener('click', function(){
   console.log(firstClick);
 });
 
+clearBtn.addEventListener('click', function(){
+  output.value = ' ';
+})
 
 
 plusBtn.addEventListener('click', function(){
